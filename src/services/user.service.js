361e1e3899b32c8607f061853e4ae86fdc4e57cf
Alejandro Gabriel Rodriguez Castro <alejandrogrc@gmail.com>
2024-02-03 18:10:45 -0300

    [ADD] User service

---
+++ src/services/user.service.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+ 
+class UserService{
+    async getUsers(){
+        try{
+            const users = await User.find();
+            return users;
+        } catch(err){
+            console.error(err);
+            throw new Error('Error al obtener los usuarios');
+        }
+    }
+
+    async getUserById(id){
+        try {
+            let user = await User.findById(id);
+            return user;
+        } catch (err) {
+            console.error(err);
+            throw new Error('Error al obtener el usuario');
+        }
+    }
+
+    async createUser(user){
+        try {
+            let isUserRegistered = await User.findOne({email: user.email});
+            if(isUserRegistered){
+                throw new Error('El usuario ya existe');
+            }
+            else{
+                let newUser = new User(user);
+                newUser.password = await bcrypt.hashSync(user.password, parseInt(process.env.SALT_ROUNDS) | 10);
+                await newUser.save();
+                return newUser;
+            }
+        } catch (err) {
+            console.error(err);
+            throw new Error('Error al crear el usuario');
+        }
+    }
+
+    async getUserByEmail(email){
+        try {
+            let user = await User.findOne({email: email});
+            console.log("getUserByEmail");
+            console.log(user);
+            return user;
+        } catch (err) {
+            console.error(err);
+            throw new Error('Error al obtener el usuario con email');
+        }
+    }
+}
+
+module.exports = new UserService();
